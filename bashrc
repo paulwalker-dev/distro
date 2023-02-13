0f9947d19258cecd3f064117500dafc1260f8795
Paul Walker <paulwalker@paulwalker.dev>
2023-02-13 17:45:37 -0500

    Add gettext (broken)

--- bashrc
+++ bashrc
@@ -4,7 +4,8 @@ if [ -d "/repo" ]; then
     KISS_PATH="/repo"
 else
     KISS_ROOT="$(pwd)/sysroot"
-    KISS_PATH="$(pwd)/repo/cross"
+    KISS_BASE="$(pwd)/repo"
+    KISS_PATH="$KISS_BASE/cross:$KISS_BASE/chroot"
 fi
 LC_ALL=POSIX
 KISS_TGT="$(uname -m)-lfs-linux-gnu"
@@ -12,7 +13,10 @@ PATH="/usr/bin"
 if [ ! -L /bin ]; then PATH="/bin:$PATH"; fi
 PATH="$KISS_ROOT/tools/bin:$PATH"
 PATH="$(pwd)/kiss:$PATH"
+if [ -d "/repo" ]; then
+    PATH="$PATH:/tmp/bin"
+fi
 CONFIG_SITE=$KISS_ROOT/usr/share/config.site
-export KISS_ROOT KISS_PATH LC_ALL KISS_TGT PATH CONFIG_SITE
+export KISS_BASE KISS_ROOT KISS_PATH LC_ALL KISS_TGT PATH CONFIG_SITE
 export MAKEFLAGS="-j12"
-export LOGNAME=$(whoami)
+export LOGNAME="builder"
---
+++ sandbox.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+kiss d $(kiss s 3* | tr '/' ' ' | gawk '{ print $NF }')
+
+mkdir -p chroot/{fakeroot,upper,work,db}
+fuse-overlayfs -o lowerdir=sysroot,upperdir=chroot/upper,workdir=chroot/work chroot/fakeroot
+
+bwrap \
+    --dev-bind chroot/fakeroot / \
+    --dev /dev \
+    --proc /proc \
+    --tmpfs /run \
+    --ro-bind "$KISS_BASE"/chroot /repo \
+    --ro-bind $(which kiss) /usr/bin/kiss \
+    --ro-bind shell.sh /root/shell.sh \
+    --ro-bind bashrc /root/.bashrc \
+    --dev-bind tmp/.cache /root/.cache \
+    --dev-bind chroot/db /var/db \
+    --symlink /usr/bin/echo /tmp/bin/wget \
+    --symlink /usr/bin/sh /tmp/bin/su \
+    --share-net \
+    --unshare-user \
+    --uid 0 \
+    --gid 0 \
+    -- /usr/bin/env -i \
+        HOME=/root \
+        TERM="$TERM" \
+        PATH=/usr/bin:/usr/sbin:/tmp/bin \
+        CHROOT=1 \
+        /root/shell.sh
+
+rm -rf chroot/fakeroot/tmp
+umount chroot/fakeroot
